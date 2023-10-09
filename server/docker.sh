4c0e53c40f5b232c414b8b7a5c8643a285b2da03
Sedixed <sedixed@gmail.com>
2023-10-10 00:17:29 +0200

    fix: removed submodule and replaced by a real directory

---
+++ server/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Migrate the database and load fixtures if the env is dev
+php bin/console d:d:c
+php bin/console d:m:m --env=dev --no-interaction
+
+php bin/console cache:clear
+
+# Start apache
+exec apache2-foreground
