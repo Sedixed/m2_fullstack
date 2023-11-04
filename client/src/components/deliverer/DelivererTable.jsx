7fc3e3e2a46289ac030465e50dc6a9df98e1e38e
Sedixed <sedixed@gmail.com>
2023-11-04 21:35:21 +0100

    feat: added deliverer edition modal and deliverer deletion client-side

--- client/src/components/deliverer/DelivererTable.jsx
+++ client/src/components/deliverer/DelivererTable.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 
-const DelivererTable = ({ deliverers }) => {
+import '../../styles/DelivererTable.css';
+
+const DelivererTable = ({ 
+    deliverers, 
+    editionCallback,
+    deletionCallback
+}) => {
     const renderedDeliverers = deliverers.map(d => {
         return (
             <tr key={d.id}>
-                <td>{d.name}</td>
-                <td><i className={`icon ${d.available ? 'checkmark green' : 'close red'}`}></i></td>
-                <td>{new Date(d.creationDate).toLocaleDateString()}</td>
+                <td className="center aligned">{d.name}</td>
+                <td className="center aligned">
+                    <i className={`icon ${d.available ? 'checkmark green' : 'close red'}`}></i>
+                </td>
+                <td className="center aligned">
+                    {new Date(d.creationDate).toLocaleDateString()}
+                </td>
+                <td className="center aligned actions">
+                    <i 
+                        className="icon pencil alternate edit"
+                        onClick={() => editionCallback(d)}
+                    ></i>
+                    <i 
+                        className="icon trash alternate delete"
+                        onClick={() => deletionCallback(d.id)}
+                    ></i>
+                </td>
             </tr>
         );
     })
     return (
-        <div className="ui container">
+        <div className="ui container deliverer-table">
             <table className="ui celled table">
                 <thead>
                     <tr>
-                        <th>Nom</th>
-                        <th>Disponible</th>
-                        <th>Date de création</th>
+                        <th className="center aligned">Nom</th>
+                        <th className="center aligned">Disponible</th>
+                        <th className="center aligned">Date de création</th>
+                        <th className="center aligned">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
--- client/src/components/deliverer/Home.jsx
+++ client/src/components/deliverer/Home.jsx
@@ -1,39 +1,87 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import ErrorMessage from "../ErrorMessage";
 import SuccessMessage from "../SuccessMessage";
 import server from "../../apis/server";
 import routes from "../../constants/routes";
 import DelivererTable from "./DelivererTable";
 import Loader from "../Loader.jsx";
+import PaginationTabs from "../PaginationTabs";
+import EditionModal from "./EditionModal";
 
 const HomePage = () => {
     const [deliverers, setDeliverers] = useState(null);
+    const [hydraView, setHydraView] = useState(null);
+    const [delivererToEdit, setDelivererToEdit] = useState(null);
+    const [fetchingParams, setFetchingParams] = useState({
+        pagination: true,
+        page: 1,
+        itemsPerPage: 8
+    });
+    const location = useLocation();
+    const modalRef = useRef();
 
     const fetch = async () => {
         const { data } = await server.get(
             routes.DELIVERERS, 
             {
-                params: {
-                    pagination: true,
-                    page: 1,
-                    itemsPerPage: 5
-                }
-            });
+                params: fetchingParams
+            });  
         setDeliverers(data['hydra:member']);
-        console.log(data);
+        setHydraView(data['hydra:view']);
+        console.log(location.state);
+    }
+    
+    const deleteDeliverer = async (delivererId) => {
+        try {
+            const data = await server.delete(
+                routes.DELIVERERS + '/' + delivererId
+            );
+            if (data.status == 204) {
+                location.state = {
+                    deleted: true
+                };
+                fetch();
+            }
+        } catch (exception) {
+            console.log(exception);
+        }
+    }
+
+    const closeModal = () => {
+        setDelivererToEdit(null);
     }
 
     useEffect(() => {
         fetch();
-    }, []);
+    }, [fetchingParams]);
 
     return (
         !deliverers ?
         <Loader /> :
         <React.Fragment>
+            {
+                delivererToEdit ?
+                <EditionModal 
+                    modalRef={modalRef} 
+                    deliverer={delivererToEdit}
+                    closeModal={closeModal}
+                    refetchCallback={fetch}
+                /> :
+                null
+            }
             <ErrorMessage />
             <SuccessMessage />
-            <DelivererTable deliverers={deliverers} />
+            <DelivererTable 
+                deliverers={deliverers} 
+                editionCallback={setDelivererToEdit}
+                deletionCallback={deleteDeliverer}
+            />
+            <PaginationTabs 
+                currentPage={fetchingParams.page}
+                hydraView={hydraView} 
+                callback={setFetchingParams}
+            />
         </React.Fragment>
     );
 };
