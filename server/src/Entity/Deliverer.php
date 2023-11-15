f9cbee86594249c4ce9e29aeae510cbad4f377b4
Neuskan <94790506+Neuskan@users.noreply.github.com>
2023-11-15 21:49:24 +0100

    fix: problem of api filters and package-lock.json

--- server/src/Entity/Deliverer.php
+++ server/src/Entity/Deliverer.php
@@ -2,12 +2,37 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Patch;
 use App\Repository\DelivererRepository;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 
 #[ORM\Entity(repositoryClass: DelivererRepository::class)]
-#[ApiResource]
+#[ORM\HasLifecycleCallbacks]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(
+            paginationType: 'page',
+            paginationClientItemsPerPage: true,
+            paginationClientEnabled: true
+        ),
+        new Post(),
+        new Patch(),
+        new Delete()
+    ]
+)]
 class Deliverer
 {
     #[ORM\Id]
@@ -16,13 +41,24 @@ class Deliverer
     private ?int $id = null;
 
     #[ORM\Column(length: 32)]
+    #[ApiFilter(OrderFilter::class)]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[ApiFilter(BooleanFilter::class)]
     private ?bool $available = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $creationDate = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ApiFilter(DateFilter::class)]
+    #[ApiFilter(OrderFilter::class)]
+    #[ApiProperty(writable: false)]
+    private ?\DateTimeInterface $creationDate = null;
+    
+    #[ORM\PrePersist]
+    public function prePersist()
+    {   
+        $this->creationDate = new DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -53,12 +89,12 @@ class Deliverer
         return $this;
     }
 
-    public function getCreationDate(): ?\DateTimeImmutable
+    public function getCreationDate(): ?\DateTimeInterface
     {
         return $this->creationDate;
     }
 
-    public function setCreationDate(\DateTimeImmutable $creationDate): static
+    public function setCreationDate(\DateTimeInterface $creationDate): static
     {
         $this->creationDate = $creationDate;
 
