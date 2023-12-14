bd85f08f6d07b00b3f64987f13e70f67fe48a0d9
Sedixed <sedixed@gmail.com>
2023-12-14 14:29:54 +0100

    fix: removed unused imports

--- client/src/components/delivery/DeliveryEditionModal.jsx
+++ client/src/components/delivery/DeliveryEditionModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import routes from "../../constants/routes";
 import server from "../../apis/server";
